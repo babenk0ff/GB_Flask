d096d4f66d3d18971e404625ebb210405d287dcd
stas <babenk0fff@gmail.com>
2023-03-28 05:47:19 -0700

    lesson-11

--- blog/views/articles.py
+++ blog/views/articles.py
@@ -20,12 +20,15 @@ articles_app = Blueprint(
 @articles_app.route('/', endpoint='list')
 def articles_list():
     tag_name = request.args.get('tag')
+    author_id = request.args.get('author_id')
     if tag_name:
         articles = Article.query.filter(Article.tags.any(name=tag_name))
+    elif author_id:
+        articles = Article.query.filter(Article.author_id == author_id)
     else:
         articles = Article.query.all()
     return render_template('articles/list.html', articles=articles,
-                           tag=tag_name)
+                           tag=tag_name, author_id=author_id)
 
 
 @articles_app.route('/<int:article_id>/', endpoint='details')
@@ -39,7 +42,47 @@ def article_details(article_id: int):
     return render_template('articles/details.html', article=article)
 
 
-@articles_app.route('/<int:article_id>/edit', methods=['GET', 'POST'],
+@articles_app.route('/create/', methods=['GET', 'POST'], endpoint='create')
+@login_required
+def create_article():
+    error = None
+    form = CreateArticleForm(request.form)
+    form.tags.choices = [
+        (tag.id, tag.name)
+        for tag in Tag.query.order_by('name')
+    ]
+    if request.method == 'POST' and form.validate_on_submit():
+        article = Article(title=form.title.data.strip(), body=form.body.data)
+
+        if current_user.author:
+            article.author_id = current_user.author.id
+        else:
+            author = Author(user_id=current_user.id)
+            db.session.add(author)
+            db.session.flush()
+            article.author_id = author.id
+
+        if form.tags.data:
+            selected_tags = Tag.query.filter(Tag.id.in_(form.tags.data))
+            for tag in selected_tags:
+                article.tags.append(tag)
+
+        db.session.add(article)
+        try:
+            db.session.commit()
+        except IntegrityError:
+            current_app.logger.exception('Could not create a new article!')
+            error = 'Could not create article!'
+        else:
+            return redirect(url_for(
+                'articles_app.details',
+                article_id=article.id),
+            )
+    return render_template('articles/create.html', form=form, error=error)
+
+
+@articles_app.route('/<int:article_id>/edit',
+                    methods=['GET', 'POST'],
                     endpoint='edit')
 @login_required
 def edit_article(article_id: int):
@@ -49,27 +92,24 @@ def edit_article(article_id: int):
     if article is None:
         raise NotFound
     if article.author_id != current_user.author.id \
-            and current_user.is_staff is not True:
+            and not current_user.is_staff:
         return abort(403)
     article_tags = [tag.name for tag in article.tags]
 
     form = EditArticleForm()
     form.title.data = article.title
     form.body.data = article.body
-    form.tags.choices = [(tag.id, tag.name) for tag in
-                         Tag.query.order_by('name')]
-    for tag in form.tags:
-        tag.default = 1
+    form_tags = [(tag.id, tag.name) for tag in
+                 Tag.query.order_by('name')]
 
-    if form.validate_on_submit():
+    if request.method == 'POST' and form.validate_on_submit():
         article.title = form.title.data.strip()
         article.body = form.body.data
-        article.tags = []
+        form_selected_tags = request.form.getlist('tags')
 
-        if form.tags.data:
-            selected_tags = Tag.query.filter(Tag.id.in_(form.tags.data))
-            for tag in selected_tags:
-                article.tags.append(tag)
+        article.tags.clear()
+        for tag in Tag.query.filter(Tag.id.in_(form_selected_tags)).all():
+            article.tags.append(tag)
 
         try:
             db.session.commit()
@@ -86,44 +126,6 @@ def edit_article(article_id: int):
         form=form,
         error=error,
         article=article,
-        selected_tags=article_tags,
+        form_tags=form_tags,
+        article_tags=article_tags,
     )
-
-
-@articles_app.route('/create/', methods=['GET', 'POST'], endpoint='create')
-@login_required
-def create_article():
-    error = None
-    form = CreateArticleForm(request.form)
-    form.tags.choices = [
-        (tag.id, tag.name)
-        for tag in Tag.query.order_by('name')
-    ]
-    if request.method == 'POST' and form.validate_on_submit():
-        article = Article(title=form.title.data.strip(), body=form.body.data)
-
-        if form.tags.data:
-            selected_tags = Tag.query.filter(Tag.id.in_(form.tags.data))
-            for tag in selected_tags:
-                article.tags.append(tag)
-
-        if current_user.author:
-            article.author_id = current_user.author.id
-        else:
-            author = Author(user_id=current_user.id)
-            db.session.add(author)
-            db.session.flush()
-            article.author_id = author.id
-
-        db.session.add(article)
-        try:
-            db.session.commit()
-        except IntegrityError:
-            current_app.logger.exception('Could not create a new article!')
-            error = 'Could not create article!'
-        else:
-            return redirect(url_for(
-                'articles_app.details',
-                article_id=article.id),
-            )
-    return render_template('articles/create.html', form=form, error=error)
--- blog/views/index.py
+++ blog/views/index.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, url_for
 
 index_app = Blueprint('index_app', __name__, url_prefix='/')
 
 
 @index_app.route('/', endpoint='index')
 def index():
-    return render_template('index.html')
+    return redirect(url_for('articles_app.list'))
